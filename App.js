fa17a10b46e8d0eabf3d528460e5a0a5f41a1ee4
dyego.batista <dyego.batista@usto.re>
2022-04-10 22:42:40 -0300

    criação da tela 1 da atividade proposta

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { Icon, Input, Button, Alert } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "collumn", backgroundColor: "#dce9e7", alignItems: "center", justifyContent: 'center', paddingBottom: 100 }}>
+      <View style={{ alignItems: "center", paddingTop: 50 }}>
+        <Icon
+          raised
+          size={60}
+          name='alien-outline'
+          type='material-community'
+          color='grey' />
+      </View>
+      <View style={{ height: 150, width: 300, paddingTop: 10 }}>
+        <Input placeholder="E-mail" />
+        <Input style={{ marginTop: 10 }} placeholder="Senha" />
+        <View style={{ flexDirection: 'row' }}>
+          <Button
+            title="Entrar"
+            buttonStyle={{
+              borderColor: '#f4f4f4',
+              backgroundColor: '#f4f4f4',
+              borderRadius: 3,
+            }}
+            containerStyle={{
+              width: 100,
+              marginHorizontal: 10,
+              marginVertical: 10,
+              marginLeft: 40,
+            }}
+            titleStyle={{ color: "grey" }}
+          />
+          <Button
+            title="Cadastrar"
+            buttonStyle={{
+              backgroundColor: '#f4f4f4',
+              borderRadius: 3,
+            }}
+            containerStyle={{
+              width: 100,
+              marginHorizontal: 10,
+              marginVertical: 10,
+            }}
+            titleStyle={{ color: "grey" }}
+          />
+        </View>
+      </View>
+      <View style={{ backgroundColor: "aqua", alignItems: "center" }}>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +57,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  label: {
+    marginRight: 10,
+  }
 });
